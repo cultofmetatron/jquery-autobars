231c32ce3209d82ddc5383e2f9584afe5962b119
cultofmetatron <cultofmetatron@aumlogic.com>
2013-03-30 22:25:11 -0700

    changed name to jquery-autobars

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-handlebar-helper
+jQuery Autobars
 ================
 
 A convenient jquery plugin for loading handlebars templates declared in script tags!
@@ -9,48 +9,50 @@ if it seems useful to you!
 Usage
 ================
 
-Handlebar Helper's sweet spot is cases where you want to have your
+jQuery-autobars's sweet spot is cases where you want to have your
 handlebars templates in their own file/files.
 
-Handlebar helper makes the distinction between partials files (which
+This plugin makes the distinction between partials files (which
 can hold multiple handlebars templates) and full templates which are one
 per file.
 
 You can load a handlebar template to load into $.handlebarTemplates.["compiled template"]
 with the script tag.
 
+```html
 	<script src="/main.hbs" type="text/x-handlebars-template"></script>
 
-handlebar helper looks at the src and downloads the code as needed, compiles it into
+Autobars looks at the src and downloads the code as needed, compiles it into
 a handlebars template and adds the method to the $.handlebarTemplates space where you can use it in your projects!!
 
 
 A partials file contains several handlebar template source files and are between
-
+```html
 	<!--#?index-->
 	  handlebars code goes here
 	<!--#?end-->
 
-Handlebars helper knows its a a partials file via a script tag with type 
+Handlebars helper knows its a a partials file via a script tag with type
 
+```html
 	<script src="/helper-templates.hbs" type="text/handlebar-partials"></script>
 
 in this case the type="text/handlebar-partials" marks the file for additional parsing.
 
-The name after the ? in the top indicates the name the template will have in 
-the partials namespace and can be accessed at the handlebarTemplates namespace 
+The name after the ? in the top indicates the name the template will have in
+the partials namespace and can be accessed at the handlebarTemplates namespace
 under jquery.
-
-	$(document).handlebarHelper(function() {
+```html
+	$(document).autoBars(function() {
 		/* you pass a callback in to perform work on the templates
 		becasuse handlebar helper is making multiple aynchrous requests
-		and it makes sure to not call this callback till all the necessary files 
+		and it makes sure to not call this callback till all the necessary files
 		are loaded into the $.handlebarsTemplates namespace.
 		*/
         console.log('it worked!!')
         var $html = $.handlebarTemplates.partials.index({
             message: "hello welcome to handlebar helper!!",
-            });
+        });
         $('body').append($html);
       });
 
---
+++ src/jquery-autobars.js
@@ -0,0 +1,119 @@
+/* this file is a basic helper utility for people
+ * using handlebars.js. it allowes you to store several
+ * handlebars templates seperated with
+ *
+ *       <!--#?templateid-->
+ *
+ *
+ *       <!--#?end-->
+ */
+
+(function($) {
+  methods = {
+
+    initialize: function() {
+        //console.log('this');
+
+    },
+    parsePartials:function(data){
+      /* this function splits the partials */
+
+      //first we seperate the strings with a regular expression
+      var seperatorToken = /<!--#\?[a-zA-Z]+-->/; //matches the headers
+      var endToken = /<!--#\?end-->/;
+      var END_TOKEN_SIZE = 12;
+      //now we get the names of the partials
+      var data = data.split('\n').join('');;
+      var templates = {};
+      //first remove all white space charachters that are in groups > 2
+      data = data.split(/\s{2,}/).join('');
+      templates = {};
+      //now that we have a whitespace stripped version, we loop throught them
+      //all chunking them.
+      //get the first token and extract the key
+      while(data.match(seperatorToken)) {
+        var token = data.match(seperatorToken)[0]; //gets the first token
+        var name = token.match(/[a-zA-Z]+/)[0];//the name we get token
+        data = data.slice(token.length);
+        //feed in characters till you get to the end tag.
+        var endIndex = data.search(endToken);
+        var source = data.slice(0,endIndex);
+        data = data.slice(endIndex + END_TOKEN_SIZE); //increment to the next size;
+        templates[name] = Handlebars.compile(source);
+      }
+      //load the, into the handlebarTemplates namespace;
+      for(key in templates) {
+        if (jQuery.handlebarTemplates[key] === undefined) {
+          //silently fails to load in a  new template if and older one exists;
+          jQuery.handlebarTemplates.partials[key] = templates[key];
+        }
+      }
+    },
+    mainTemplates: function(context) {
+      var pipe = [];//promise objects
+      context.find('[type="text/x-handlebars-template"]').each(function(index, element) {
+        var loadUrl = $(element).attr('src');
+        var name = loadUrl.match(/[a-zA-Z]+/);
+        //here we gather all our promises
+        pipe.push($.get(loadUrl, function(data) {
+          jQuery.handlebarTemplates[name] = Handlebars.compile(data);
+        }));
+      });
+      console.log('main templates', pipe);
+      return pipe;
+    },
+    partials:  function(context) {
+      //we take the nodes and pull out partials
+      var pipe = [];//promise objects
+      context.find('[type="text/handlebar-partials"]').each(function(index, element) {
+        //handlebarTemplates = Handlebars.compile($(element).html());
+        var loadUrl = $(element).attr('src');
+        //gather the promises
+         pipe.push($.get(loadUrl, function(data) {
+          //each pageload is performed asynchronously and so the data exists only in this
+          //context. here we scrub the input and use it;
+          methods.parsePartials(data);
+        }));
+      });
+
+      console.log('partials', pipe)
+
+      return pipe;
+    },
+  };
+  //heres where it begins
+  jQuery.fn.autoBars = function(options, callback) {
+    args = Array.prototype.slice.call(arguments, 0);
+    if (args.length = 1 && typeof(args[0]) == 'function') {
+        //checks if theres only one argument and sets the callback to be the first
+        callback = options;
+        options = {};
+    }
+
+    jQuery.handlebarTemplates = jQuery.handlebarTemplates || {}; //so we don't overwrite it
+    jQuery.handlebarTemplates.partials = jQuery.handlebarTemplates.partials || {};
+    var settings = $.extend({
+      loadHandlebars : false,
+    }, options);
+
+    // gather all the promises from the multiple async calls
+    var partialPromises   = methods.partials(this);
+    var templatesPromises = methods.mainTemplates(this);
+    var promises = partialPromises.concat(templatesPromises);
+    console.log(promises);
+
+    // we delay exection of the callback until all the promises are fullfilled!!
+    if (typeof(callback) === 'function') {
+        $.when.apply(this, promises).done(callback);
+    }
+
+
+    return this // return the original jquery object
+  }
+
+  jQuery.fn.hbh = jQuery.fn.handlebarHelper
+})(jQuery);
+
+
+
+
